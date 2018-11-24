1f359ffea68064ee591f133fc837eb86e076db4a
sl4vkek <43294802+sl4vkek@users.noreply.github.com>
2018-11-24 13:14:45 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Module: default
+# Author: Roman V. M.
+# Created on: 28.11.2014
+# License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
+
+import sys
+from urllib import urlencode
+from urlparse import parse_qsl
+import xbmcgui
+import xbmcplugin
+
+xbmc.Player().play("https://dvr.ch20-cdnwiz.com/hls/dvr.m3u8")
